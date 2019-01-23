1122120a215be0060207507d65f51ce195105be6
carissa morrow <carissamorrow@hotmail.com>
2019-01-23 14:24:22 -0700

    grrr....

--- wwwsrc/src/views/Vault.vue
+++ wwwsrc/src/views/Vault.vue
@@ -2,12 +2,12 @@
   <div class="vaultKeep container">
     <div class="row">
       <div class="col">
-        <div v-for="vaultKeep in VaultKeeps" class="card count col-4" v-show="vaultKeep.userId == user.id">
+        <div v-for="vaultKeep in vaultKeeps" class="card count col-4">
           <!-- this should show all of the vaultKeeps (keeps added to this vault ) -->
-          <!-- <router-link :to="{name: 'vault', params: {vaultId: vault.id}}"> -->
-          <p class="textSpace mt-3">{{vault.name}}</p>
-          <p class="textSpace">{{vault.description}}</p>
-          <!-- </router-link> -->
+          <router-link :to="{name: 'vaultKeep', params: {vaultId: vault.id}}">
+            <p class="textSpace mt-3">{{keep.name}}</p>
+            <p class="textSpace">{{keep.description}}</p>
+          </router-link>
         </div>
       </div>
     </div>
@@ -24,10 +24,14 @@
       }
     },
     computed: {
-
+      vaultKeeps() {
+        return this.$store.state.vaultKeeps
+      }
     },
     methods: {
-
+      getKeepsByVaultId() {
+        this.$store.dispatch("getKeepsByVaultId", vaultId)
+      }
     }
   }
 
